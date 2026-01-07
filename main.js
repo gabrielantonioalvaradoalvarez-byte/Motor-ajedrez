cd4d6fba4623dc9922b5a80fe2e68c8588140df7
gabrielantonioalvaradoalvarez-byte <gabrielantonioalvaradoalvarez@gmail.com>
2026-01-06 18:20:41 -0600

    Initialize chess game and board with drag-and-drop

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
+const game = new Chess();
+
+const board = Chessboard('board', {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+});
+
+function onDragStart(source, piece) {
+  // bloquear si la partida terminó
+  if (game.game_over()) return false;
+
+  // solo permitir mover el color que toca
+  if (
+    (game.turn() === 'w' && piece.startsWith('b')) ||
+    (game.turn() === 'b' && piece.startsWith('w'))
+  ) {
+    return false;
+  }
+}
+
+function onDrop(source, target) {
+  const move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // luego hacemos selector
+  });
+
+  if (move === null) return 'snapback';
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
 
